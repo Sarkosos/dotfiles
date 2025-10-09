6ed6b88bf61a8bbd3fcaa06b169e29cf12a0d4c5
Sarkosos <szymonfonau@live.nl>
2025-10-09 18:31:40 +0200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,28 +2,48 @@
 require("config.lazy")
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
--- vim.lsp.config("basedpyright", {
---   root_markers = { ".git", { "pyproject.toml" } },
---   settings = {
---     basedpyright = {
---       analysis = {
---         autoSearchPaths = true,
---         diagnosticMode = "openFilesOnly",
---         useLibraryCodeForTypes = true,
---         typeCheckingMode = "basic",
---         diagnosticSeverityOverrides = {
---           reportAny = false,
---           reportMissingTypeArgument = false,
---           reportMissingTypeStubs = false,
---           reportUnknownArgumentType = false,
---           reportUnknownMemberType = false,
---           reportUnknownParameterType = false,
---           reportUnknownVariableType = false,
---           reportUnusedCallResult = false,
+vim.lsp.config("basedpyright", {
+  root_markers = { ".git", { "pyproject.toml" } },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "basic",
+        diagnosticSeverityOverrides = {
+          reportAny = false,
+          reportMissingTypeArgument = false,
+          reportMissingTypeStubs = false,
+          reportUnknownArgumentType = false,
+          reportUnknownMemberType = false,
+          reportUnknownParameterType = false,
+          reportUnknownVariableType = false,
+          reportUnusedCallResult = false,
+        },
+        ignore = { ".venv" },
+      },
+    },
+  },
+})
+vim.lsp.enable("basedpyright")
+-- local servers = {
+--   -- clangd = {},
+--   -- gopls = {},
+--   -- pyright = {},
+--   basedpyright = {
+--     settings = {
+--       basedpyright = {
+--         analysis = {
+--           useLibraryCodeForTypes = true,
+--           typeCheckingMode = "basic",
+--           diagnosticMode = "openFilesOnly",
+--           autoSearchPath = true,
+--           inlayHints = {
+--             callArgumentNames = true,
+--           },
 --         },
---         ignore = { ".venv" },
 --       },
 --     },
 --   },
--- })
--- vim.lsp.enable("basedpyright")
+-- }
